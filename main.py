0184b0a823d4432367ee2bc8edff66853ca5fc05
aduno <aduno7647@gmail.com>
2024-03-11 10:32:34 -0400

    Accepting html and htm files. Fixed error reponse with error codes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.responses import StreamingResponse
 from uocalendar.uocalendar import UOCalendar
 import logging
@@ -22,16 +22,16 @@ def generate_ics(file: UploadFile = File(...)):
 
     logging.basicConfig(level=logging.INFO)
     if not file:
-        return {"message": "No upload file sent"}
+        raise HTTPException(status_code=400, detail="No upload file sent")
     # Check if file is an htm file
-    elif not file.filename.endswith('.htm'):
-        return {"message": "File is not an htm file"}
+    elif not file.filename.endswith('.htm') and not file.filename.endswith('.html'):
+        raise HTTPException(status_code=400, detail="File is not an htm file") 
     try:
         contents = file.file.read()
         ics = uocalendar.run(contents.decode('utf-8'))
         return StreamingResponse(ics, media_type="text/calendar", headers={"Content-Disposition": "attachment; filename=My Schedule.ics"})
     except Exception as e:
-        return {"message": f"An error occured: {str(e)}"}
+        raise HTTPException(status_code=400, detail=f"An error occured: {str(e)}")
 
 @app.post("/errors")
 def log_error(file: UploadFile = File(...)):
